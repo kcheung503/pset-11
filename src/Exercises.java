22dd8d2c7a17835de5d08cf8452e68218e673c60
kyle <kwoodc503@gmail.com>
2020-03-12 08:55:19 -0400

    adding exercise names

--- src/Exercises.java
+++ src/Exercises.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Exercises {
 
+//Exercise 1
   public int findMe(int[] list, int target) {
     if (list == null || list.length == 0) {
       return -1;
@@ -16,6 +17,7 @@ public class Exercises {
     return -1;
   }
 
+//Exercise 2
   public int findMe(ArrayList<String> list, String target) {
     if (list == null || list.size() == 0 || target == null) {
       return -1;
@@ -30,42 +32,52 @@ public class Exercises {
     return -1;
   }
 
+//Exercise 3
   public int findMeFaster(ArrayList<Integer> list, int target) {
     return -1;
   }
 
+//Exercise 4
   public int findMeFaster(String[] list, String target) {
     return -1;
   }
 
+//Exercise 5
   public int[] bubble(int[] list, boolean ascending) {
     return null;
   }
 
+//Exercise 6
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
     return null;
   }
 
+//Exercise 7
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
     return null;
   }
 
+//Exercise 8
   public String[] insertion(String[] list, boolean ascending) {
     return null;
   }
 
+//Exercise 9
   public int[] selection(int[] list, boolean ascending) {
     return null;
   }
 
+//Exercise 10
   public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
     return null;
   }
 
+//Exercise 11
   public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
     return null;
   }
 
+//Exercise 12nigger
   public String[] merge(String[] list, boolean ascending) {
     return null;
   }
